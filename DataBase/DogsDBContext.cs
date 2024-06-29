b54e600288c43d12803edd9e2f7d2a3da882ee4a
Sashok9203 <kovalchuk.olex@gmail.com>
2024-06-29 18:06:14 +0300

    Добавьте файлы проекта.

---
+++ DataBase/DogsDBContext.cs
@@ -0,0 +1,16 @@
+﻿using DataBase.Data.Entities;
+using DataBase.Data.Entities.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBase
+{
+	public class DogsDBContext(DbContextOptions<DogsDBContext> options) : DbContext(options)
+	{
+		public DbSet<Dog> Dogs { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration<Dog>(new DogConfig());
+		}
+
+	}
+}
